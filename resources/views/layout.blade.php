277fad1ed7b0f367fa68fa6a90481ac80d88d0cb
Campelo Dev Pro <campelodevpro>
2024-08-31 07:34:13 -0300

    CRUD FINAL PRODUTO: Aplicado Tailwind - Concluido

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,12 @@
 <html>
 <head>
     <title>CRUD de Produtos</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+
+   
 </head>
 <body class="bg-gray-100 text-gray-900">
 
